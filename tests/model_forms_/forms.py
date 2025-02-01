eefcb2f84b8f6b947bd01a42fab477679e9963a9
Tim Graham <timograham@gmail.com>
2025-01-31 19:12:07 -0500

    INTPYTHON-483 Add EmbeddedModelArrayField

--- tests/model_forms_/forms.py
+++ tests/model_forms_/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Author, Book
+from .models import Author, Book, Movie, Store
 
 
 class AuthorForm(forms.ModelForm):
@@ -13,3 +13,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         fields = "__all__"
         model = Book
+
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        fields = "__all__"
+        model = Movie
+
+
+class StoreForm(forms.ModelForm):
+    class Meta:
+        fields = "__all__"
+        model = Store
