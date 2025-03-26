4dd83032273ba25c9d55380518e1204263c5cc19
Tim Graham <timograham@gmail.com>
2025-03-25 20:26:22 -0400

    rename field

--- django_mongodb_backend/fields/__init__.py
+++ django_mongodb_backend/fields/__init__.py
@@ -2,15 +2,15 @@ from .array import ArrayField
 from .auto import ObjectIdAutoField
 from .duration import register_duration_field
 from .embedded_model import EmbeddedModelField
+from .embedded_model_array import EmbeddedModelArrayField
 from .json import register_json_field
-from .multiple_embedded_model import MultipleEmbeddedModelField
 from .objectid import ObjectIdField
 
 __all__ = [
     "register_fields",
     "ArrayField",
+    "EmbeddedModelArrayField",
     "EmbeddedModelField",
-    "MultipleEmbeddedModelField",
     "ObjectIdAutoField",
     "ObjectIdField",
 ]
