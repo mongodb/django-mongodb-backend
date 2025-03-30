60910ae6db8895307690741f8f995f3f78698ff8
Tim Graham <timograham@gmail.com>
2025-03-29 20:58:20 -0400

    Add support for AddEmbeddedIndex

--- django_mongodb_backend/models.py
+++ django_mongodb_backend/models.py
@@ -3,11 +3,16 @@ from django.db import NotSupportedError, models
 from .managers import EmbeddedModelManager
 
 
+class EMBEDDED:
+    pass
+
+
 class EmbeddedModel(models.Model):
     objects = EmbeddedModelManager()
 
     class Meta:
         abstract = True
+        db_table = EMBEDDED
 
     def delete(self, *args, **kwargs):
         raise NotSupportedError("EmbeddedModels cannot be deleted.")
