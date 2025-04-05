f602a7dffc7ff44cc28206ecfa023129b7ebeb6c
Tim Graham <timograham@gmail.com>
2025-04-05 08:29:14 -0400

    Add AggregateFilter, StringgAgg.as_mql() as per https://github.com/django/django/commit/4b977a5d7283e7ca51288cc0ed0860e0004653ca

--- django_mongodb_backend/features.py
+++ django_mongodb_backend/features.py
@@ -89,6 +89,12 @@ class DatabaseFeatures(BaseDatabaseFeatures):
         # This backend overrides DatabaseCreation.create_test_db() so the
         # deprecation warnings stacklevel points to the wrong file.
         "backends.base.test_creation.TestDbCreationTests.test_serialize_deprecation",
+        # StringAgg is not supported.
+        "aggregation.tests.AggregateTestCase.test_distinct_on_stringagg",
+        "aggregation.tests.AggregateTestCase.test_string_agg_escapes_delimiter",
+        "aggregation.tests.AggregateTestCase.test_string_agg_filter",
+        "aggregation.tests.AggregateTestCase.test_string_agg_filter_in_subquery",
+        "aggregation.tests.AggregateTestCase.test_stringagg_default_value",
     }
     # $bitAnd, #bitOr, and $bitXor are new in MongoDB 6.3.
     _django_test_expected_failures_bitwise = {
