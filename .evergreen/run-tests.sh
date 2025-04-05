17f706652129de081bf26a497a88f6086c62415e
Tim Graham <timograham@gmail.com>
2025-04-04 21:52:15 -0400

    Update to Django 6.0

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -3,13 +3,13 @@
 set -eux
 
 # Install django-mongodb-backend
-/opt/python/3.10/bin/python3 -m venv venv
+/opt/python/3.12/bin/python3 -m venv venv
 . venv/bin/activate
 python -m pip install -U pip
 pip install -e .
 
 # Install django and test dependencies
-git clone --branch mongodb-5.2.x https://github.com/mongodb-forks/django django_repo
+git clone --branch mongodb-6.0.x https://github.com/mongodb-forks/django django_repo
 pushd django_repo/tests/
 pip install -e ..
 pip install -r requirements/py3.txt
