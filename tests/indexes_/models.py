0594101fa8727687f00f708444132931a3db6082
Emanuel Lupi <emanuel.lupi91@gmail.com>
2025-04-15 23:16:02 -0300

    Remove unused field

--- tests/indexes_/models.py
+++ tests/indexes_/models.py
@@ -15,5 +15,4 @@ class Article(models.Model):
     data = models.JSONField()
     embedded = EmbeddedModelField(Data)
     created_at = models.DateTimeField(auto_now=True)
-    title_embedded = ArrayField(models.FloatField(), size=10)
     description_embedded = ArrayField(models.DecimalField(decimal_places=3, max_digits=10), size=10)
