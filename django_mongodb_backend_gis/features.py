302017334b53d5dbe08d0b5f0c566f78660f92cc
Tim Graham <timograham@gmail.com>
2025-05-29 16:24:52 -0400

    add back GeometryCollection, initialize with srid=4326

--- django_mongodb_backend_gis/features.py
+++ django_mongodb_backend_gis/features.py
@@ -16,27 +16,19 @@ class DatabaseFeatures(BaseSpatialFeatures, MongoFeatures):
                 # SRIDs aren't populated: AssertionError: 4326 != None
                 # self.assertEqual(4326, nullcity.point.srid)
                 "gis_tests.geoapp.tests.GeoModelTest.test_proxy",
-                # GEOSException: Calling transform() with no SRID set is not supported
+                # MongoDB does not support the within lookup
                 "gis_tests.relatedapp.tests.RelatedGeoModelTest.test06_f_expressions",
                 # 'Adapter' object has no attribute 'srid'
                 "gis_tests.geoapp.test_expressions.GeoExpressionsTests.test_geometry_value_annotation",
                 # Object of type ObjectId is not JSON serializable
                 "gis_tests.geoapp.test_serializers.GeoJSONSerializerTests.test_fields_option",
-                # source and target must be of type SpatialReference
                 "gis_tests.geoapp.test_serializers.GeoJSONSerializerTests.test_geometry_field_option",
-                "gis_tests.geoapp.test_serializers.GeoJSONSerializerTests.test_id_field_option",
                 "gis_tests.geoapp.test_serializers.GeoJSONSerializerTests.test_serialization_base",
                 "gis_tests.geoapp.test_serializers.GeoJSONSerializerTests.test_srid_option",
-                # GeometryCollection not supported
+                # Crash initializing GeometryCollection: 'tuple' object is not callable
                 "gis_tests.geoapp.tests.GeoModelTest.test_geometryfield",
-                "gis_tests.geogapp.tests.GeographyTest.test05_geography_layermapping",
-                "gis_tests.layermap.tests.LayerMapTest.test_layermap_unique_multigeometry_fk",
-                "gis_tests.layermap.tests.LayerMapTest.test_null_geom_with_unique",
-                "gis_tests.layermap.tests.LayerMapTest.test_test_fid_range_step",
                 # KeyError: 'within' connection.ops.gis_operators[self.lookup_name]
                 "gis_tests.geoapp.tests.GeoModelTest.test_gis_query_as_string",
-                # Point object != Point object
-                "gis_tests.geoapp.test_expressions.GeoExpressionsTests.test_update_from_other_field",
                 # No lookups are supported (yet?)
                 "gis_tests.geoapp.tests.GeoLookupTest.test_gis_lookups_with_complex_expressions",
             }
