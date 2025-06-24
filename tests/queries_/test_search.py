8537a006cf1925f3a7fb6d9d8c606f6fefcd9e56
Emanuel Lupi <emanuel.lupi91@gmail.com>
2025-06-24 11:23:25 -0300

    Add test

--- tests/queries_/test_search.py
+++ tests/queries_/test_search.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 
-from .models import Article
-
 from django_mongodb_backend.functions import SearchEquals
+
+from .models import Article
 
 
 class SearchTests(TestCase):
     def test_1(self):
         Article.objects.create(headline="cross", number=1, body="body")
         aa = Article.objects.annotate(score=SearchEquals(path="headline", value="cross")).all()
-        print(aa)
+        self.assertEqual(aa.score == 1)
+        # print(aa)
