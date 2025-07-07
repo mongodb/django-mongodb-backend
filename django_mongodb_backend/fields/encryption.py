9a3c18ef9c2a722eb54e8dc11dab0d7d33d3e6c5
Jeffrey A. Clark <aclark@aclark.net>
2025-07-07 16:22:14 -0400

    Update django_mongodb_backend/fields/encryption.py
    Co-authored-by: Tim Graham <timograham@gmail.com>

--- django_mongodb_backend/fields/encryption.py
+++ django_mongodb_backend/fields/encryption.py
@@ -5,6 +5,6 @@ class EncryptedCharField(models.CharField):
     encrypted = True
     queries = []
 
-    def __init__(self, *args, **kwargs):
-        self.queries = kwargs.pop("queries", [])
+    def __init__(self, *args, queries=None, **kwargs):
+        self.queries = queries
         super().__init__(*args, **kwargs)
