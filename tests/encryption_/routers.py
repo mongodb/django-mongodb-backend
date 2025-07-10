2780e329fd21fa1a9fdd6e2c9d50a1b1ba5dad9a
Jeffrey A. Clark <aclark@aclark.net>
2025-07-10 16:53:26 -0400

    Fix test router

--- tests/encryption_/routers.py
+++ tests/encryption_/routers.py
@@ -2,17 +2,12 @@
 
 
 class TestEncryptedRouter:
+    def allow_migrate(self, db, app_label, model_name=None, model=None, **hints):
+        return getattr(model, "encrypted", False)
+
     def db_for_read(self, model, **hints):
         if getattr(model, "encrypted", False):
             return f"{model.db_name}"
         return None
 
-    def db_for_write(self, model, **hints):
-        if getattr(model, "encrypted", False):
-            return f"{model.db_name}"
-        return None
-
-    def allow_migrate(self, db, app_label, model_name=None, model=None, **hints):
-        if getattr(model, "encrypted", False):
-            return f"{model.db_name}"
-        return None
+    db_for_write = db_for_read
