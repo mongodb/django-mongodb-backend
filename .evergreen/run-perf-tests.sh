14dec036d1288490cf37a05f7d2c4d1f1fb26353
Noah Stapp <noah.stapp@mongodb.com>
2025-08-13 12:41:21 -0400

    INTPYTHON-847 Add performance tests

---
+++ .evergreen/run-perf-tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+set -eux
+
+export OUTPUT_FILE="results.json"
+
+# Install django-mongodb-backend
+/opt/python/3.12/bin/python3 -m venv venv
+. venv/bin/activate
+python -m pip install -U pip
+pip install -e .
+
+python .evergreen/run_perf_test.py
+mv performance_tests/$OUTPUT_FILE $OUTPUT_FILE
+mv performance_tests/report.json report.json
---
+++ performance_tests/perftest/settings.py
@@ -0,0 +1,13 @@
+# Django settings for the performance tests.
+DATABASES = {
+    "default": {
+        "ENGINE": "django_mongodb_backend",
+        "NAME": "benchmarking",
+    },
+}
+
+SECRET_KEY = "django_tests_secret_key"  # noqa: S105
+
+INSTALLED_APPS = ["perftest"]
+
+DEFAULT_AUTO_FIELD = "django_mongodb_backend.fields.ObjectIdAutoField"
