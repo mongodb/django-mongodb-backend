ad70f5964b79bb8250a8b2d767c95d638c584aef
Jeffrey A. Clark <aclark@aclark.net>
2025-10-28 18:58:35 -0400

    Fix pagination
    Existing approaches to "no count" pagination do not seem to include
    support for pagination in the Django admin, so:
    
    - Subclass Django's core Paginator
      - override count
    
    - Subclass Django's contrib admin view ChangeList.get_results
      - Use len instead of count
      - Ruff fixes for try/except
    
    - Subclass Django's contrib ModelAdmin
      - override get_paginator
      - override get_changelist

--- django_mongodb_backend/admin.py
+++ django_mongodb_backend/admin.py
@@ -1,32 +1,56 @@
 from django.contrib import admin
+from django.contrib.admin.options import IncorrectLookupParameters
 from django.contrib.admin.views.main import ChangeList
+from django.core.paginator import InvalidPage, Paginator
+from django.utils.functional import cached_property
 
 
-class EncryptedPaginator:
-    # TODO: Implement pagination for encrypted data. This paginator
-    # currently returns all results in a single page.
-    def __init__(self, queryset, per_page):
-        self.queryset = queryset
-        self.per_page = per_page
-
-    def page(self, number):
-        results = list(self.queryset)
-        has_next = False
-        return results, has_next
+class EncryptedPaginator(Paginator):
+    @cached_property
+    def count(self):
+        return len(self.object_list)
 
 
 class EncryptedChangeList(ChangeList):
     def get_results(self, request):
-        paginator = EncryptedPaginator(self.queryset, self.list_per_page)
-        self.result_list, _ = paginator.page(self.page_num + 1)
-
-        self.result_count = len(self.result_list)
-        self.full_result_count = self.result_count
-
-        self.can_show_all = True
-        self.multi_page = False
+        """
+        This is django.contrib.admin.views.main.ChangeList.get_results with
+        a single modification to avoid COUNT queries.
+        """
+        paginator = self.model_admin.get_paginator(request, self.queryset, self.list_per_page)
+        result_count = paginator.count
+        if self.model_admin.show_full_result_count:
+            # Modification: avoid COUNT query by using len() on the root queryset
+            full_result_count = len(self.root_queryset)
+        else:
+            full_result_count = None
+        can_show_all = result_count <= self.list_max_show_all
+        multi_page = result_count > self.list_per_page
+        if (self.show_all and can_show_all) or not multi_page:
+            result_list = self.queryset._clone()
+        else:
+            try:
+                result_list = paginator.page(self.page_num).object_list
+            except InvalidPage as err:
+                raise IncorrectLookupParameters from err
+        self.result_count = result_count
+        self.show_full_result_count = self.model_admin.show_full_result_count
+        self.show_admin_actions = not self.show_full_result_count or bool(full_result_count)
+        self.full_result_count = full_result_count
+        self.result_list = result_list
+        self.can_show_all = can_show_all
+        self.multi_page = multi_page
+        self.paginator = paginator
 
 
 class EncryptedModelAdmin(admin.ModelAdmin):
+    """
+    A ModelAdmin that uses EncryptedPaginator and EncryptedChangeList
+    to avoid COUNT queries in the admin changelist.
+    """
+
+    def get_paginator(self, request, queryset, per_page):
+        return EncryptedPaginator(queryset, per_page)
+
     def get_changelist(self, request, **kwargs):
         return EncryptedChangeList
