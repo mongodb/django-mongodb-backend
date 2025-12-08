736678e89d0814087954d7f7f3ca73f8b837c0d5
Tim Graham <timograham@gmail.com>
2025-12-08 11:36:33 -0500

    Fix class name in error message when Func is missing as_mql()

--- tests/db_functions_/tests.py
+++ tests/db_functions_/tests.py
@@ -5,6 +5,6 @@ from django.test import SimpleTestCase
 
 class FuncTests(SimpleTestCase):
     def test_no_as_mql(self):
-        msg = "Func() may need an as_mql() method."
+        msg = "Func may need an as_mql() method."
         with self.assertRaisesMessage(NotSupportedError, msg):
             Func().as_mql(None, None)
