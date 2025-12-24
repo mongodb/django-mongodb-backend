881d6c9c92e92ea1bc85160d83063e386dfe8b7c
Tim Graham <timograham@gmail.com>
2025-12-23 19:43:17 -0500

    Use DatabaseFeatures.supports_inspectdb

--- django_mongodb_backend/features.py
+++ django_mongodb_backend/features.py
@@ -35,6 +35,7 @@ class DatabaseFeatures(GISFeatures, BaseDatabaseFeatures):
     supports_expression_indexes = False
     supports_foreign_keys = False
     supports_ignore_conflicts = False
+    supports_inspectdb = False
     supports_json_field_contains = False
     # BSON Date type doesn't support microsecond precision.
     supports_microsecond_precision = False
@@ -294,10 +295,6 @@ class DatabaseFeatures(GISFeatures, BaseDatabaseFeatures):
         "Casting datetime/timedelta literals has microsecond differences.": {
             "db_functions.comparison.test_cast.CastTests.test_cast_from_python_to_datetime",
             "db_functions.comparison.test_cast.CastTests.test_cast_to_duration",
-        },
-        "inspectdb is not supported.": {
-            "inspectdb.tests.InspectDBTestCase",
-            "inspectdb.tests.InspectDBTransactionalTests",
         },
         "DatabaseIntrospection.get_table_description() not supported.": {
             "introspection.tests.IntrospectionTests.test_bigautofield",
