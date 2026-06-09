5bd0413c9bc372b76a5e90511cf8461b7c2efce2
Steven Silvester <steve.silvester@mongodb.com>
2026-06-09 06:11:12 -0500

    INTPYTHON-771 Use 'from None' in rest_framework import guard

--- django_mongodb_backend/rest_framework/__init__.py
+++ django_mongodb_backend/rest_framework/__init__.py
@@ -9,7 +9,7 @@ except ModuleNotFoundError as exc:
         raise ModuleNotFoundError(
             "djangorestframework is required to use django_mongodb_backend.rest_framework. "
             "Install it with: pip install 'django-mongodb-backend[rest_framework]'"
-        ) from exc
+        ) from None
     raise
 
 __all__ = ["EmbeddedModelSerializer", "MongoModelSerializer", "PolymorphicEmbeddedModelSerializer"]
